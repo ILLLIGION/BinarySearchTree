9a089d5327f437c331b90d142341b37d9cfa45b0
Nikita Datsuk <ILLLIGION@users.noreply.github.com>
2016-09-30 15:45:57 +0300

    Update BinarySearchTree.h

--- include/BinarySearchTree.h
+++ include/BinarySearchTree.h
@@ -4,26 +4,63 @@
 template <typename T>
 class BinarySearchTree {
 public:
-	struct Node {
-		Node(T value) : value_(value), left_(nullptr), right_(nullptr) {}
-		T value_;
-		Node * left_;
-		Node * right_;
-		friend std::ostream& operator<<(std::ostream& out, const Node& node)
-		{
-			out << node.value_ << ' ';
-			if (node.left_) out << *node.left_;
-			if (node.right_) out << *node.right_;
-			return out;
-		}
-		~Node()
-		{
-			if (left_)
-				delete left_;
-			if (right_)
-				delete right_;
-		};
-	};
+					struct Node {
+						Node(T value) : value_(value), left_(nullptr), right_(nullptr) {}
+						auto copy(Node* node) -> Node*
+						{
+							value_ = node->value_;
+							delete left_;
+							delete left_;
+							if (node->left_)
+								left_ = new Node(node->left_->value_);
+							else
+								left_ = nullptr;
+							if (node->right_)
+								right_ = new Node(node->right_->value_);
+							else
+								right_ = nullptr;
+							if (left_)
+								left_ = left_->copy(node->left_);
+							if (right_)
+								right_ = right_->copy(node->right_);
+							return this;
+						}
+						Node * left_;
+						Node * right_;
+						T value_;
+						friend std::ostream& operator<<(std::ostream& out, const Node& node)
+							{
+							out << node.value_ << ' ';
+							if (node.left_) out << *node.left_;
+							if (node.right_) out << *node.right_;
+							return out;
+							}
+						auto compare (Node* node) const noexcept -> bool
+						{
+
+							bool equalityL = true, equalityR = true;
+							if ((left && !node->left) || (right && !node->right) || (!left && node->left) || (!right && node->right) 
+																												|| (value != node->value))
+								return false;
+							else
+							{
+								if (!left && !right)
+									return true;
+								if (left)
+									equalityL = left->equal(node->left);
+								if (right)
+									equalityR = right->equal(node->right);
+								return equalityL && equalityR;
+							}
+						}
+
+						~Node()
+						{
+							delete left_; left_ = nullptr;
+							if (right_) delete right_;
+						};
+					};
+
 	BinarySearchTree() : root_(nullptr), size_(0) {};
 
 	BinarySearchTree(const std::initializer_list<T> & list) : BinarySearchTree()
@@ -32,13 +69,27 @@ public:
 			insert(value);
 	};
 
+	BinarySearchTree(const BinarySearchTree& tree) : size_(tree.size_), root_(nullptr)
+	{
+		root_ = new Node(0);
+		root_ = root_->copy(tree.root_);
+	}
+
+	BinarySearchTree(BinarySearchTree&& tree) : size_(tree.size_), root_(nullptr)
+	{
+		root_ = tree.root;
+		tree.root = nullptr;
+		tree.size = 0;
+	}
+
 	~BinarySearchTree()
 	{
 		delete root_;
 		size_ = 0;
 	};
-	auto size() noexcept->size_t
+	auto size() const noexcept->size_t
 	{
+		size_ = 0;
 		return size_;
 	};
 	auto insert(const T & value) noexcept -> bool
@@ -55,7 +106,7 @@ public:
 		{
 			prevNode = curNode;
 			if (value == curNode->value_) return false;
-			else if (value < curNode->value_) curNode = curNode->left_;
+			if (value < curNode->value_) curNode = curNode->left_;
 			else curNode = curNode->right_;
 		}
 		if (value < prevNode->value_) prevNode->left_ = new Node(value);
@@ -76,23 +127,65 @@ public:
 	return nullptr;
 	};
 
-	friend std::ostream & operator << (std::ostream & out, const BinarySearchTree<T> & tree)
+	auto RCL(std::ostream& out, Node* node) const noexcept -> bool
 	{
-		if (tree.size_) out << *tree.root_;
-		return out;
-	};;
+		if (node)
+		{
+			RCL(out, node->right_);
+			out << node->value_ << ' ';
+			RCL(out, node->left_);
+			return true;
+		}
+		else return false;
+	}
 
-	friend std::istream & operator >> (std::istream & in, BinarySearchTree<T> & tree)
+	auto CLR(std::ofstream& out, Node* node) const noexcept -> bool
+	{
+		if (node)
+		{
+			out << node->value_ << ' ';
+			CLR(out, node->left_);
+			CLR(out, node->right_);
+			return true;
+		}
+		else return false;
+	}
+
+	friend auto operator << (std::ostream& out, const BinarySearchTree<T>& tree) -> std::ostream&
+	{
+		tree.RCL(out, tree.root_);
+		return out;
+	}
+
+	friend auto operator << (std::ofstream& out, const BinarySearchTree<T>& tree) -> std::ofstream&
+	{
+		tree.CLR(out, tree.root_);
+		return out;
+	}
+
+	friend auto operator >> (std::istream& in, BinarySearchTree<T>& tree) -> std::istream&
 	{
 		T value;
 		while (in >> value) tree.insert(value);
 		return in;
+	}
+	friend auto operator >> (std::ifstream& in, BinarySearchTree<T>& tree) -> std::ifstream&
+	{
+		T value;
+		while (in >> value) tree.insert(value);
+		return in;
+	}
+
+	auto operator == (const BinarySearchTree& tree) -> bool
+	{
+		if (root->compare(tree.root))
+			return true;
+		else
+			return false;
 	};
 
 private:
 	Node * root_;
 	size_t size_;
-
-	
 };
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
