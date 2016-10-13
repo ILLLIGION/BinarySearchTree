0de63c504e9469ed2e88af6ad433f42f81c5db6d
Nikita Datsuk <ILLLIGION@users.noreply.github.com>
2016-10-13 10:47:47 +0300

    Update remove.cpp

--- tests/remove.cpp
+++ tests/remove.cpp
@@ -52,36 +52,35 @@ SCENARIO("The Node doesn't have any children")
 	}
 }
 
-SCENARIO("The Node has two children")
+SCENARIO("remove element that has something on the left side AND something on the right side")
 {
-	GIVEN("Node with two children")
+	GIVEN("some tree")
 	{
-		BinarySearchTree<int> tree1{10,5,15,2,7,6,1};
-		BinarySearchTree<int> tree2{10,6,7,15,2,1};
-		WHEN("remove")
+		BinarySearchTree<int> tree{20, 10, 9, 26, 24, 27, 22, 23, 25, 21, 28};
+		BinarySearchTree<int> tree2{20, 10, 9, 26, 27, 22, 23, 25, 21, 28};
+		WHEN("remove element")
 		{
-			tree1.remove(5);
-			THEN("Node is destroyed")
+			tree.remove(24);
+			THEN("element was removed")
 			{
-				REQUIRE(tree1==tree2);
-			}
+				REQUIRE(tree2 == tree);
+			}			
 		}
 	}
 }
-
-SCENARIO("Node we want to remove - is root")
+SCENARIO("remove root")
 {
-	GIVEN("tree")
+	GIVEN("some tree")
 	{
-		BinarySearchTree<int> tree1{4,2,1,3,5};
-		BinarySearchTree<int> tree2{3,2,1,5};
-		WHEN("remove")
+		BinarySearchTree<int> tree{4, 2, 1, 3, 5};
+		BinarySearchTree<int> tree2{2, 1, 3, 5};
+		WHEN("remove root")
 		{
-			tree1.remove(4);
-			THEN("root was removed. We have a new root")
+			tree.remove(4);
+			THEN("element was removed")
 			{
-				REQUIRE(tree1==tree2);
-			}
+				REQUIRE(tree2 == tree);
+			}			
 		}
 	}
 }
