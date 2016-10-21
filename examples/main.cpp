e556da2c6ad01f99fdeaefdee86c33a6dc0cb8b6
Nikita Datsuk <ILLLIGION@users.noreply.github.com>
2016-10-21 13:05:27 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,12 +1,39 @@
 #include "../include/BinarySearchTree.h"
 
-	int main(int argc, char* argv[])
+int main(int argc, char* argv[])
+{
+	BinarySearchTree<char> Tree1{ 'q', 'w', 'e', 'r', 't', 'y' };
+	std::cout << Tree1 << std::endl;
+
+	BinarySearchTree<double> Tree2{ 2.3, 4.2, 3.2, 5.32 };
+	std::cout << Tree2 << std::endl;
+
+	try
 	{
-		BinarySearchTree<char> Tree1{ 'q', 'w', 'e', 'r', 't', 'y' };
-		std::cout << Tree1 << std::endl;
-
-		BinarySearchTree<double> Tree2{ 2.3, 4.2, 3.2, 5.32 };
-		std::cout << Tree2 << std::endl;
-
-		return 0;
+		Tree2.insert(2.3);
 	}
+	catch(logical_error& error)
+	{
+		std::cerr << error.what() << std::endl;
+	}
+	try
+	{
+		Tree2.find(100);
+	}
+	catch (logical_error& error)
+	{
+		std::cerr << error.what() << std::endl;
+	}
+	try
+	{
+		Tree2.remove(100);
+	}
+	catch (logical_error& error)
+	{
+		std::cerr << error.what() << std::endl;
+	}
+
+	system("pause");
+	return 0;
+}
+
